6fc1ac18a476cf20b86af614b1ff491e57205480
kme-developer <electruc0095@gmail.com>
2025-08-03 12:04:05 +0900

    feature. add payment domain.

---
+++ apps/payment/src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { PaymentModule } from './payment/payment.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: 'apps/payment/.env',
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.getOrThrow('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    PaymentModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ apps/payment/src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { PaymentModule } from './payment/payment.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(PaymentModule);
+  await app.listen(process.env.port ?? 3000);
+}
+
+bootstrap();
